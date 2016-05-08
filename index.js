d9077ff287345c908a699a8b68ad89bb378e94ff
Frank Lemanschik <frank@github.dspeed.eu>
2016-05-08 15:32:11 +0100

    Implemented .jade Compatible layer for dug rendering

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
-const jade = require('jade')
+const dug = require('dug')
 
 /*
  * Usage:
- * var jade = require('diet-jade')({ path: app.path+'/static/jade' })
- *
- * app.header(jade)
+ * var dug = require('diet-dug')({ path: app.path+'/static/dug' })
+ * var jade = require('diet-dug')({ path: app.path+'/static/jade' })
+ * app.header(dug)
  *
  * $.render('index') => $.render('index.jade')
  *
@@ -18,7 +18,13 @@ module.exports = function(options) {
     $.render = function(filename) {
       $.header('Content-Type', 'text/html; charset=UTF-8')
       console.log(filename);
-      filename ? (filename.indexOf('.jade')>-1 ? (options.file = filename) : (options.file = filename + '.jade')) : ($.error('No jade file specified'))
+      // give depricated warning if .jade is used
+      
+      // filename var is set and yeld WARNING if it is set to .jade
+      filename ? (filename.indexOf('.jade')>-1 ? (console.log('WARNING: Will try to render .dug file NOT .jade IT is DEPRICATED!')) : ($.error('No .dug file specified'))
+      // check if filename is dug or jade if not append dug
+      filename.indexOf('.dug')>-1 ? (options.file = filename) : (filename.indexOf('.jade')>-1 ? (options.file = filename) : (options.file = filename. + '.dug'))
+      
       var path = (options.path.slice(-1) === '/') ? options.path : options.path + '/'
       var fn = jade.compileFile(path + options.file, {
         pretty: true,

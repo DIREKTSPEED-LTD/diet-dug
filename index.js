68b488c8043db2134a3c88e9f5fdefb6d274d09d
Frank Lemanschik <frank@github.dspeed.eu>
2016-05-08 16:23:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,37 @@
 
 // Dependencies
 var fs = require('fs')
-var dug = require('dug')
+var pug = require('pug')
 var merge = require('merge')
 var clone = require('clone')
 
 module.exports = function(options){
 	
 	var options = options || {}
-	var renderer = ect(merge({ 
+	var renderer = pug(merge({ 
 		root : options.path, 
-		ext: '.html', 
+		ext: '.pug', 
 		open: '{{', close: '}}',
 		cache: true,
 		watch: true,
 		gzip: true,
+		pretty: true,
+		compileDebug: false,
 	}, options))
 	
 	return function($){
 		$.htmlModule = function(pathname){
-		    if(!pathname || (pathname && pathname.indexOf(/\n|\r/) != -1)){
-    			pathname ? ( pathname.indexOf('.dug')>-1 ? ( var path = pathname ) : ( var path = pathname + '.dug' ) ) : var path = 'index.dug'
-    			// var path = pathname ? pathname : 'index.dug' 
-    			var context = merge(clone($, false, 1), $.data)
-    			var html = renderer.render(path, context)
+		    //check if file exist in r
+		    var context = merge(clone($, false, 1), $.data)
+		    // if has new line or start with < 
+		    // will fail if your filename has < in it (need other way to check if it is a html string)
+		    if(!pathname || (pathname && pathname.indexOf(/\n|\r|</) != -1)){
+    			pathname ? ( pathname.indexOf('.pug')>-1 ? ( var path = pathname ) : ( var path = pathname + '.pug' ) ) : var path = 'index.pug'
+    			// var path = pathname ? pathname : 'index.pug'
+    			var html = renderer.renderFile(path, context)
     			$.response.end(html)
 			} else if (pathname) {
-			    $.response.end(pathname)
+			    renderer.render(pathname, context)
 			}
 			
 			$.nextRoute() // call next route
